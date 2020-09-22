98df545a65d6922472d8973fa9a2836b2ece68da
Ditwan Price <price.ditwan@gmail.com>
2020-09-22 13:12:00 -0500

    Adding back the frontend files.

---
+++ frontend/homebase_react/src/containers/TodoListView.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+import TodoListComponent from '../components/TodoListComponent';
+
+class TodoListView extends React.Component {
+
+state = {
+    todoListData: []
+}
+
+fetchTodos = () => {
+    axios.get('http://127.0.0.1:8000/api/').then(response => {
+        this.setState({
+            todoListData: response.data
+        });
+    });
+}
+
+componentDidMount(){
+    this.fetchTodos();
+}
+
+render(){
+    return ( 
+        <div className="todo-component-div">
+            <TodoListComponent todos={this.state.todoListData} getTodos={this.fetchTodos}/>
+        </div>
+    );
+}
+};
+
+export default TodoListView;
