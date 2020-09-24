db43d825b144e05e90382863e52e1da3b34a8ceb
Ditwan Price <price.ditwan@gmail.com>
2020-09-24 12:39:37 -0500

    Added component for welcome message.

---
+++ frontend/homebase_react/src/components/welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class welcome extends React.Component {
+    render() {
+        return(
+            <div className="welcome-message">
+                <h1>Welcome Back!</h1>
+            </div>
+        )
+    }
+}
+
+export default welcome;
