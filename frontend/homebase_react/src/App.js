b15725732a6f3d58605a53360cb91f65a8b03195
Ditwan Price <price.ditwan@gmail.com>
2020-09-24 12:39:37 -0500

    Added component for welcome message.

--- frontend/homebase_react/src/App.js
+++ frontend/homebase_react/src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
-import  {BrowserRouter as Router, Route} from 'react-router-dom';
+import  {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import TodoListView from './containers/TodoListView';
 import TodoInputForm from './components/TodoInputForm';
 import NavBar from './components/NavBar';
+import welcome from './components/welcome';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
@@ -15,10 +16,15 @@ class App extends Component {
   render(){
     return (
       <Router>
+        <NavBar />
         <div className="App">
-          <NavBar />
-          <Route path="/todo" component={TodoInputForm} />
-          <Route path="/todo" component={TodoListView} />
+          <Switch>
+            <Route exact path="/" component={welcome}/>
+            <Route path="/todo"> 
+              <TodoInputForm />
+              <TodoListView />
+            </Route>
+          </Switch>
         </div>
       </Router>
     );
