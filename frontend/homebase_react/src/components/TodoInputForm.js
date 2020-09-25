919ed9f80085af17e1f78a37a5134eb317bec8b0
Ditwan Price <price.ditwan@gmail.com>
2020-09-25 15:33:47 -0500

    Added simple animations using react-spring.

--- frontend/homebase_react/src/components/TodoInputForm.js
+++ frontend/homebase_react/src/components/TodoInputForm.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 import { FormGroup, Form, Button, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Spring } from 'react-spring/renderprops';
+
 
 class TodoInputForm extends React.Component {
 
@@ -40,56 +42,64 @@ class TodoInputForm extends React.Component {
     }
 
     render() {
-        return ( 
-            // Bootsrap form group for the input of the todo data.
-            <div className="input-form-container">
-                <Form noValidate>
-                    <Form.Row bsPrefix="title-row">
-                        <Col bsPrefix="input-columns">
-                            <FormGroup bsPrefix="todo-title-form">
-                                <input 
-                                    as="input" 
-                                    className="todo-title-input"
-                                    type="title"
-                                    size="sm" 
-                                    onChange={this.handleTitleChange}
-                                    value={this.state.title}
-                                    required
-                                    autoComplete="off"
-                                />
-                                <label htmlFor="todo-title-input" className="title-label"> 
-                                    <span className="title-span"> Title: </span> 
-                                </label>
-                            </FormGroup>
-                        </Col>
-                    </Form.Row>
-                    <Form.Row bsPrefix="description-row">
-                        <Col bsPrefix="input-columns">
-                            <FormGroup bsPrefix="todo-description-form">
-                                <input 
-                                    className="todo-description-input"
-                                    onChange={this.handleContentChange} 
-                                    value={this.state.content} 
-                                    required
-                                    autoComplete="off"
-                                /> 
-                                <label htmlFor="todo-description-input" className="description-label"> 
-                                    <span className="description-span"> Description: </span>
-                                </label>
-                            </FormGroup>
-                            <Form.Group bsPrefix="add-todo-button-container">
-                                <Button 
-                                    type="submit"
-                                    variant="primary" 
-                                    onClick={this.handleSubmit} 
-                                    bsPrefix='add-todo-button'> 
-                                    <FontAwesomeIcon icon="plus"/>
-                                </Button>
-                            </Form.Group>
-                        </Col>
-                    </Form.Row>
-                </Form>
-            </div>
+        return (
+            <Spring 
+                from={{ marginTop: -500}} 
+                to={{ marginTop: 175}}
+                config={{ duration: 1000}}
+            >
+                {props => ( 
+                    // Bootsrap form group for the input of the todo data.
+                    <div style={props} className="input-form-container">
+                        <Form noValidate>
+                            <Form.Row bsPrefix="title-row">
+                                <Col bsPrefix="input-columns">
+                                    <FormGroup bsPrefix="todo-title-form">
+                                        <input 
+                                            as="input" 
+                                            className="todo-title-input"
+                                            type="title"
+                                            size="sm" 
+                                            onChange={this.handleTitleChange}
+                                            value={this.state.title}
+                                            required
+                                            autoComplete="off"
+                                        />
+                                        <label htmlFor="todo-title-input" className="title-label"> 
+                                            <span className="title-span"> Title: </span> 
+                                        </label>
+                                    </FormGroup>
+                                </Col>
+                            </Form.Row>
+                            <Form.Row bsPrefix="description-row">
+                                <Col bsPrefix="input-columns">
+                                    <FormGroup bsPrefix="todo-description-form">
+                                        <input 
+                                            className="todo-description-input"
+                                            onChange={this.handleContentChange} 
+                                            value={this.state.content} 
+                                            required
+                                            autoComplete="off"
+                                        /> 
+                                        <label htmlFor="todo-description-input" className="description-label"> 
+                                            <span className="description-span"> Description: </span>
+                                        </label>
+                                    </FormGroup>
+                                    <Form.Group bsPrefix="add-todo-button-container">
+                                        <Button 
+                                            type="submit"
+                                            variant="primary" 
+                                            onClick={this.handleSubmit} 
+                                            bsPrefix='add-todo-button'> 
+                                            <FontAwesomeIcon icon="plus"/>
+                                        </Button>
+                                    </Form.Group>
+                                </Col>
+                            </Form.Row>
+                        </Form>
+                    </div>
+                )}
+            </Spring>
     );}
 }
 
--- frontend/homebase_react/src/components/welcome.js
+++ frontend/homebase_react/src/components/welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Spring } from 'react-spring/renderprops';
 
 class welcome extends React.Component {
     state = {
@@ -19,20 +20,24 @@ class welcome extends React.Component {
         this.fetchTodaysQuote();
     }
 
-    // noAuthor = () => {
-    //     if
-    // }
-
     render() {
         return(
-            <div className="welcome-message">
-                {this.state.quoteData.map( quote =>
-                    <ul key={quote.id}>
-                        <h2>{quote.quote}</h2>
-                        <h3> Author - {quote.author ? quote.author : 'Somebody'}</h3>
-                    </ul>
+            <Spring 
+                from={{ marginTop: -500}} 
+                to={{ marginTop: 350}}
+                config={{duration: 1000}}
+            >
+                {props => (
+                    <div style={props} className="welcome-message">                      
+                        {this.state.quoteData.map( quote =>
+                            <ul key={quote.id}>
+                                <h2>{quote.quote}</h2>
+                                <h3> Author - {quote.author ? quote.author : 'Somebody'}</h3>
+                            </ul>
+                        )}
+                    </div>
                 )}
-            </div>
+            </Spring>
         )
     }
 }
