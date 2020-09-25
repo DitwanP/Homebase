4705501cbcdf330c6056b16d347ed37ec04928d9
Ditwan Price <price.ditwan@gmail.com>
2020-09-25 13:47:45 -0500

    Removed quote title, changed the quote font size.

--- frontend/homebase_react/src/components/welcome.js
+++ frontend/homebase_react/src/components/welcome.js
@@ -28,7 +28,6 @@ class welcome extends React.Component {
             <div className="welcome-message">
                 {this.state.quoteData.map( quote =>
                     <ul key={quote.id}>
-                        <h1>{quote.title} - </h1>
                         <h2>{quote.quote}</h2>
                         <h3> Author - {quote.author ? quote.author : 'Somebody'}</h3>
                     </ul>
