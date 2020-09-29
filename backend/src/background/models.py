9fb90e4dac37a4d1783f9f010db1b2918ef3f198
Ditwan Price <price.ditwan@gmail.com>
2020-09-29 13:56:07 -0500

    Adjustments to bg Django model.

--- backend/src/background/models.py
+++ backend/src/background/models.py
@@ -3,8 +3,10 @@ import uuid
 
 class bgImage(models.Model):
     imagePath = 'Users/wanted/Documents/Coding/homebase/backend/src/background/images/bgImage.jpg'
-    
-    image = models.ImageField(upload_to=imagePath)
+    defaultImagePath= '/Users/wanted/Documents/Coding/homebase/backend/src/background/images/default.jpg'
+
+    name = models.CharField(max_length=30, default='Placeholder name')
+    image = models.ImageField(upload_to=imagePath, default=defaultImagePath)
 
     def __str__(self):
         return self.content
