d6ae028eab246edf7ae27111a8745531a020c4a4
Ditwan Price <price.ditwan@gmail.com>
2020-10-02 11:00:36 -0500

    Got the bg pulling from the api.

--- frontend/homebase_react/src/components/BgImage.js
+++ frontend/homebase_react/src/components/BgImage.js
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
+import axios from 'axios';
+import FileSaver from 'file-saver'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class BgImage extends Component {
-    // state = {
-    //     bgOfTheDay: []
-    // }
+    state = {
+        bgOfTheDay: []
+    }
     
-    // fetchTodaysBg = () => {
-    //     axios.get('https://source.unsplash.com/random/1920x1080').then(response => {
-    //         console.log(response)
-    //         this.setState({
-    //             bgOfTheDay: response.data
-    //         });
-    //     });
-    // }
+    fetchNewBg = () => {
+        var imageBlob = FileSaver.saveAs("https://source.unsplash.com/random/1920x1080", "bgImage.jpg");
 
-    // componentDidMount(){
-    //     this.fetchTodaysBg();
-    // }
+        axios.put('http://127.0.0.1:8000/api/background/1/update/', 
+        {name: 'Current background', image: imageBlob})
+
+        axios.get('http://127.0.0.1:8000/api/background/1/').then(response => {
+            this.setState({
+                bgOfTheDay: response.data.image
+            });
+        });
+    }
+
+    componentDidMount(){
+        this.fetchNewBg();
+    }
 
     render() {
         return (
-            <div className="BgImage">
-                <img src='https://source.unsplash.com/random/1920x1080' alt=''></img>
+            <div>
+                <div className="BgImage">
+                    <img src={this.state.bgOfTheDay} alt=''></img>
+                </div>
+                <div className="button-container">
+                    <button type='submit' title="New Background" className='wallpaper-button' onClick={() => this.fetchNewBg()}> 
+                        <p> 
+                            <FontAwesomeIcon icon="redo-alt" />    
+                        </p> 
+                    </button>
+                </div>
             </div>
         )
     }
