28da374938b614fe69d396778e7cd03c9971df79
Ditwan Price <price.ditwan@gmail.com>
2020-10-09 16:51:53 -0500

    misc

--- homebase/settings.py
+++ homebase/settings.py
@@ -6,7 +6,7 @@ from boto.s3.connection import S3Connection
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = S3Connection(os.getenv['SECRET_KEY'])
+SECRET_KEY = S3Connection(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
