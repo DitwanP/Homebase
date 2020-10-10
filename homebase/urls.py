a585ff525e3cde52ddf0f53eaee97df915a23c8f
Ditwan Price <price.ditwan@gmail.com>
2020-10-10 12:02:05 -0500

    Added path to urls, and build folder for frontend.

--- homebase/urls.py
+++ homebase/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/todo/', include('todoList.api.urls')),
+    re_path('.*', TemplateView.as_view(template_name='index.html'))
 ]
