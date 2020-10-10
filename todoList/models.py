cd9bd3134743ae728c3718d5ed794eb6bc5efbfe
Ditwan Price <price.ditwan@gmail.com>
2020-10-10 18:28:03 -0500

    make it better

--- todoList/models.py
+++ todoList/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-import uuid 
+
 
 class toDo(models.Model):
-    
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=60, default='Placeholder title')
     content = models.CharField(max_length=240)
     isComplete = models.BooleanField(default=False)
