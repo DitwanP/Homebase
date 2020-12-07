f85293e861cfbbf3b623c782ecd5e290e7507dca
DitwanP <price.ditwan@gmail.com>
2020-12-07 00:03:14 -0500

    General responsive changes for multiple screens.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -99,40 +99,84 @@ position: fixed;
 top: 0;
 left: 0;
 width: 100%;
+display: flex;
+justify-content: space-between;
 margin: 0 auto;
 height: 6rem;
 background-color: rgba(0, 0, 0, 0.3);
 z-index: 1000;
 
-ul {
-    line-height: 6rem;
-    list-style: none;
-    overflow: hidden;
-    color: #fff;
-    text-align: right;
-    margin: 0.7rem 7.6rem;
-    transition: 1s;
+.site-links {
+    width: 20%;
 }
 
-ul li {
-    display: inline-block;
+ul {
+    width: 100%;
+    height: 100%;
+    list-style: none;
+    color: #fff;
+    text-align: center;
+    transition: 1s;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 0rem;
 }
 
 ul li a {
     text-decoration: none;
     color: #fff;
-    font-size: 2.6rem;
-    margin-left: 2rem;
+    font-size: 2.5rem;
+}
+
+.logo {
+    display: flex; 
+    justify-content: center;
+    align-items: center;
 }
 
 .brand {
     font-family: 'Montserrat', sans-serif;
-    line-height: 60px;
     float: left;
     margin: 0rem 6.6rem;
     color: #fff;
     font-weight: normal;
     font-size: 2.2rem;
     letter-spacing: 3px;
+}
+
+@media screen and (max-width:1000px) {
+    .site-links {
+        width: 30%;
+    }
+
+    ul li a {
+        font-size: 2rem;
+    }
+}
+
+@media screen and (max-width:480px) {
+    height: 5rem;
+    display: flex;
+    justify-content: space-between;
+
+    .site-links {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 50%;
+    }
+
+    .brand {
+        font-size: 1.3rem;
+        margin: 0rem 1rem;
+    }
+
+    ul li a {
+        text-decoration: none;
+        color: #fff;
+        font-size: 1.6rem;
+    }
 }
 `;
