880351ac244bfe2854a35d93b619b0b20890a695
DitwanP <price.ditwan@gmail.com>
2021-05-02 20:05:04 -0400

    Came back to fix some css mistakes.

--- src/components/TodoInputForm.js
+++ src/components/TodoInputForm.js
@@ -1,105 +1,106 @@
-import React from 'react';
-import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import { FormGroup, Form, Button, Col } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-class TodoInputForm extends React.Component {
+const TodoInputForm = () =>{
 
-    constructor(props) {
-        super(props);
-        this.state = { title: "", content: ""};
-    
-        this.handleInputChange = this.handleTitleChange.bind(this);
-        this.handleContentChange = this.handleContentChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+    const [todos, setTodos] = useState([]);
+    const [todo, setTodo] = useState("");
+
+    useEffect(() => {
+        const todoData = localStorage.getItem("todos");
+        const loadedTodos = JSON.parse(todoData);
+
+        if (loadedTodos) {
+            setTodos(loadedTodos);
+        }
+    }, []);
+
+    useEffect(() => {
+        const todosJson = JSON.stringify(todos);
+        localStorage.setItem("todos", todosJson)
+    }, [todos]);
+
+    const updateTodo = e => {
+        setTodo(e.target.value);
     };
 
-    // This will handle the change of the content on the title input box.
-    handleTitleChange = (event) => {
-        this.setState({
-            title: event.target.value, 
-        });
-    }
+    function handleSubmit(e) {
+        e.preventDefault()
+        
+        const newTodo = {
+            id: new Date().getTime(),
+            text: todo,
+            completed: false
+        }
 
-    // This will handle the change of the content on the description input box.
-    handleContentChange = (event) => {
-        this.setState({
-            content: event.target.value
-        });
-    }
-    
-    // This will tell the api to create a new todo with the given information 
-    // from the title and content boxes.
-    handleSubmit = (event) => {
-        event.preventDefault()
-        const currentTitle = this.state.title;
-        const currentContent = this.state.content;
+        setTodos([...todos].concat(newTodo))
+        setTodo("")
+    };
 
-        axios.post('https://homebase-rd.herokuapp.com/api/todo/create/', { title: currentTitle, content: currentContent });
-            
-        this.setState({ title: '', content: ''})
-        this.props.refreshTodos()
-    }
+    function deleteTodo(id) {
+        const updatedTodos = [...todos].filter((todo) => todo.id !== id)
 
-    componentDidMount() {
-        this.props.refreshTodos();
-    }
+        setTodos(updatedTodos);
+    };
 
-    render() {
-        return (
-            // Bootsrap form group for the input of the todo data.
-            <div className="input-form-container">
-                <Form noValidate>
-                    <Form.Row bsPrefix="title-row">
-                        <Col bsPrefix="input-columns">
-                            <FormGroup bsPrefix="todo-title-form">
-                                <input 
-                                    as="input" 
-                                    className="todo-title-input"
-                                    label="todo title"
-                                    type="title"
-                                    size="sm" 
-                                    onChange={this.handleTitleChange}
-                                    value={this.state.title}
-                                    required
-                                    autoComplete="off"
-                                />
-                                <label htmlFor="todo-title-input" className="title-label"> 
-                                    <span className="title-span"> Title: </span> 
-                                </label>
-                            </FormGroup>
-                        </Col>
-                    </Form.Row>
-                    <Form.Row bsPrefix="description-row">
-                        <Col bsPrefix="input-columns">
-                            <FormGroup bsPrefix="todo-description-form">
-                                <input 
-                                    className="todo-description-input"
-                                    label="todo description"
-                                    onChange={this.handleContentChange} 
-                                    value={this.state.content} 
-                                    required
-                                    autoComplete="off"
-                                /> 
-                                <label htmlFor="todo-description-input" className="description-label"> 
-                                    <span className="description-span"> Description: </span>
-                                </label>
-                            </FormGroup>
-                            <Form.Group bsPrefix="add-todo-button-container">
-                                <Button 
-                                    type="submit"
-                                    variant="primary" 
-                                    onClick={this.handleSubmit} 
-                                    bsPrefix='add-todo-button'
-                                    aria-label="add to todo list"> 
-                                    <FontAwesomeIcon icon="plus"/>
-                                </Button>
-                            </Form.Group>
-                        </Col>
-                    </Form.Row>
-                </Form>
-            </div>
-        )}
+    function toggleComplete(id) {
+        const updatedTodos = [...todos].map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo
+        })
+
+        setTodos(updatedTodos)
+    };
+
+    return (
+        // Bootsrap form group for the input of the todo data.
+        <div className="input-form-container">
+            <Form noValidate onSubmit={handleSubmit} className="todo-form">
+                <Form.Row bsPrefix="description-row">
+                    <Col bsPrefix="input-columns">
+                        <FormGroup bsPrefix="todo-description-form">
+                            <input 
+                                className="todo-description-input"
+                                label="todo description"
+                                onChange={updateTodo} 
+                                value={todo}
+                                required
+                                autoComplete="off"
+                            /> 
+                            <label htmlFor="todo-description-input" className="description-label"> 
+                                <span className="description-span"> Todo: </span>
+                            </label>
+                        </FormGroup>
+                        <Form.Group bsPrefix="add-todo-button-container">
+                            <Button 
+                                type="submit"
+                                variant="primary" 
+                                bsPrefix='add-todo-button'
+                                aria-label="add to todo list"> 
+                                <FontAwesomeIcon icon="plus"/>
+                            </Button>
+                        </Form.Group>
+                    </Col>
+                </Form.Row>
+            </Form>
+            {todos.map((todo) => 
+                <div className="todo-component-div" key={todo.id} value={todo.completed}>
+                    <div className="todo-content" value={todo.completed}>{todo.text}</div>
+                    <div className="btn-container"> 
+                        <button className="todo-complete-btn" value={todo.completed} onClick={() => toggleComplete(todo.id)}>
+                            <FontAwesomeIcon icon="check" />
+                        </button>
+                        <button className="todo-delete-btn" onClick={() => deleteTodo(todo.id)}>
+                            <FontAwesomeIcon icon="trash-alt" />
+                        </button>
+                    </div>
+                </div>)
+            }
+        </div>
+    )
 }
 
 export default TodoInputForm;
--- src/containers/TodoListView.js
+++ src/containers/TodoListView.js
@@ -1,38 +1,17 @@
 import React from 'react';
-import axios from 'axios';
-import TodoListComponent from '../components/TodoListComponent';
 import TodoInputForm from '../components/TodoInputForm';
 import {animateScroll as scroll} from 'react-scroll';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 class TodoListView extends React.Component {
-
-state = {
-    todoListData: []
-}
-
-fetchTodos = (event) => {
-    axios.get('https://homebase-rd.herokuapp.com/api/todo').then(response => {
-        this.setState({
-            todoListData: response.data.sort((a,b) => a.id - b.id)
-        });
-    });
-}
-
-componentDidMount(){
-    this.fetchTodos();
-}
-
 render(){
     return ( 
         <div className="todo-list">
-            <div className="BgImage2">
-            </div>
             <button className="go-to-top-button" onClick={() => scroll.scrollToTop()} aria-label="scroll to top of page">
                 <FontAwesomeIcon icon={['fas', 'chevron-up']} />
             </button>
-            <TodoInputForm refreshTodos={this.fetchTodos}/>
-            <TodoListComponent todos={this.state.todoListData} getTodos={this.fetchTodos}/>
+            <TodoInputForm/>
+            <div className="BgImage2"></div>
         </div>
     );
 }
